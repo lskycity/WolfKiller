9af5bbb8a6b330c3508930d72b00e773d9f3f319
lskycity <lskycity@foxmail.com>
2016-03-02 07:40:02 +0800

    add RoleDistributionInfo.getRandomRoleList()

--- app/src/main/java/com/dream/yzbb/wolfkiller/entity/RoleDistributionInfo.java
+++ app/src/main/java/com/dream/yzbb/wolfkiller/entity/RoleDistributionInfo.java
@@ -1,9 +1,11 @@
 package com.dream.yzbb.wolfkiller.entity;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Created by kevinbest on 16/2/27.
@@ -22,6 +24,24 @@ public class RoleDistributionInfo {
 
     public HashMap<Role, Integer> getDistribution() {
         return distribution;
+    }
+
+    private List<Role> getRandomRoleList() {
+        ArrayList<Role> list = new ArrayList<>(playerCount);
+        for(Map.Entry<Role, Integer> entry : distribution.entrySet()) {
+            addRoleToList(list, entry.getKey(), entry.getValue());
+        }
+        if(playerCount != list.size()) {
+            throw new RuntimeException("playerCount should be same with distribution size playerCount="+playerCount+", distribution="+list.size());
+        }
+        Collections.shuffle(list);
+        return list;
+    }
+
+    private void addRoleToList(ArrayList<Role> list, Role role, final int count) {
+        for(int i=0; i<count; i++) {
+            list.add(role);
+        }
     }
 
     @Override
