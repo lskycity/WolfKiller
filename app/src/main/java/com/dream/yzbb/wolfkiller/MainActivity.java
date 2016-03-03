a67d42ed598ecb1a20a4c0ed047ac67bec9a57e7
lskycity <lskycity@foxmail.com>
2016-03-03 22:59:52 +0800

    add display all player in main screen and night event

--- app/src/main/java/com/dream/yzbb/wolfkiller/MainActivity.java
+++ app/src/main/java/com/dream/yzbb/wolfkiller/MainActivity.java
@@ -1,60 +1,47 @@
 package com.dream.yzbb.wolfkiller;
 
 import android.content.Intent;
-import android.content.res.XmlResourceParser;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import com.dream.yzbb.wolfkiller.app.commonui.AboutUsActivity;
+import com.dream.yzbb.wolfkiller.entity.NightRole;
+import com.dream.yzbb.wolfkiller.service.GameManager;
 import com.dream.yzbb.wolfkiller.ui.SettingsActivity;
-import com.dream.yzbb.wolfkiller.apputils.XmlUtils;
-import com.dream.yzbb.wolfkiller.ui.SetupNewGameActivity;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView mTextView;
-    public static final String PLAYER_COUNT = "player_count";
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private GridView mGridView;
+    private TextView mDispaly;
+
+    private GameManager mGameManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mGridView = (GridView) findViewById(R.id.all_player);
+        mDispaly = (TextView) findViewById(R.id.event_panel);
+        findViewById(R.id.event_button).setOnClickListener(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        mTextView = (TextView) findViewById(R.id.role_content);
+        mGameManager = Factory.get().getGameManager();
+        mGridView.setAdapter(new PlayerAdapter(this, mGameManager.getAllPlayers()));
 
-        //Get all players' role information
-//        Factory.get().getGameManager().getAllPlayers();
-        //Then call NightRoundManager's method to do actions
+        mGameManager.getNightRoundManager().startNightRound();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         //noinspection SimplifiableIfStatement
         if (id == R.id.menu_settings) {
@@ -76,9 +63,16 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(this, SettingsActivity.class));
     }
 
-    private void openSetupNewGameActivity() {
-        startActivity(new Intent(this, SetupNewGameActivity.class));
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.event_button) {
+            if(mGameManager.getNightRoundManager().hasNext()) {
+                NightRole role = mGameManager.getNightRoundManager().nextRole();
+                mDispaly.append(role.getName()+"\n");
+            } else {
+                mGameManager.getNightRoundManager().endNightRound();
+                mDispaly.append("-------------------\n");
+            }
+        }
     }
-
-
 }
---
+++ app/src/main/java/com/dream/yzbb/wolfkiller/PlayerAdapter.java
@@ -0,0 +1,49 @@
+package com.dream.yzbb.wolfkiller;
+
+import android.app.Activity;
+import android.support.v4.view.LayoutInflaterCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.dream.yzbb.wolfkiller.entity.Player;
+
+import java.util.List;
+
+/**
+ * Created by liuzhaofeng on 2016/3/3.
+ */
+public class PlayerAdapter extends BaseAdapter {
+    private List<Player> mPlayerList;
+    private LayoutInflater mLayoutInflater;
+    /*package*/ PlayerAdapter(Activity activity, List<Player> playerList) {
+        mPlayerList = playerList;
+        mLayoutInflater = LayoutInflater.from(activity);
+    }
+    @Override
+    public int getCount() {
+        return mPlayerList.size();
+    }
+
+    @Override
+    public Player getItem(int position) {
+        return mPlayerList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return getItem(position).getPlayID();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null) {
+            convertView = mLayoutInflater.inflate(R.layout.item_main_grid_player, parent, false);
+        }
+        TextView tv = (TextView) convertView.findViewById(R.id.player_no);
+        tv.setText(String.valueOf(position)+"\n"+getItem(position).getRole().getName());
+        return convertView;
+    }
+}
